proudofyou9/bevy

commit c439cae85569161b8601caa2111a33c8e02f0f75

Migrate to encase from crevice (#4339)
# Objective

- Unify buffer APIs
- Also see #4272

## Solution

- Replace vendored `crevice` with `encase`

---

## Changelog

Changed `StorageBuffer`
Added `DynamicStorageBuffer`
Replaced `UniformVec` with `UniformBuffer`
Replaced `DynamicUniformVec` with `DynamicUniformBuffer`

## Migration Guide

### `StorageBuffer`

removed `set_body()`, `values()`, `values_mut()`, `clear()`, `push()`, `append()`
added `set()`, `get()`, `get_mut()`

### `UniformVec` -> `UniformBuffer`

renamed `uniform_buffer()` to `buffer()`
removed `len()`, `is_empty()`, `capacity()`, `push()`, `reserve()`, `clear()`, `values()`
added `set()`, `get()`

### `DynamicUniformVec` -> `DynamicUniformBuffer`

renamed `uniform_buffer()` to `buffer()`
removed `capacity()`, `reserve()`


Co-authored-by: Carter Anderson <mcanders1@gmail.com>

crates/bevy_pbr/src/render/mesh.rs
@@ -1,6 +1,6 @@
 use crate::{
-    GlobalLightMeta, GpuLights, LightMeta, NotShadowCaster, NotShadowReceiver, ShadowPipeline,
-    ViewClusterBindings, ViewLightsUniformOffset, ViewShadowBindings,
+    GlobalLightMeta, GpuLights, GpuPointLights, LightMeta, NotShadowCaster, NotShadowReceiver,
+    ShadowPipeline, ViewClusterBindings, ViewLightsUniformOffset, ViewShadowBindings,
     CLUSTERED_FORWARD_STORAGE_BUFFER_COUNT,
 };
 use bevy_app::Plugin;
@@ -19,7 +19,7 @@ use bevy_render::{
     render_asset::RenderAssets,
     render_component::{ComponentUniforms, DynamicUniformIndex, UniformComponentPlugin},
     render_phase::{EntityRenderCommand, RenderCommandResult, TrackedRenderPass},
-    render_resource::{std140::AsStd140, *},
+    render_resource::*,
     renderer::{RenderDevice, RenderQueue},
     texture::{BevyDefault, GpuImage, Image, TextureFormatPixelInfo},
     view::{ComputedVisibility, ViewUniform, ViewUniformOffset, ViewUniforms},
@@ -76,7 +76,7 @@ impl Plugin for MeshRenderPlugin {
     }
 }
 
-#[derive(Component, AsStd140, Clone)]
+#[derive(Component, ShaderType, Clone)]
 pub struct MeshUniform {
     pub transform: Mat4,
     pub inverse_transpose_model: Mat4,
@@ -267,10 +267,7 @@ impl FromWorld for MeshPipeline {
         let render_device = world.resource::<RenderDevice>();
         let clustered_forward_buffer_binding_type = render_device
             .get_supported_read_only_binding_type(CLUSTERED_FORWARD_STORAGE_BUFFER_COUNT);
-        let cluster_min_binding_size = match clustered_forward_buffer_binding_type {
-            BufferBindingType::Storage { .. } => None,
-            BufferBindingType::Uniform => BufferSize::new(16384),
-        };
+
         let view_layout = render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
             entries: &[
                 // View
@@ -280,7 +277,7 @@ impl FromWorld for MeshPipeline {
                     ty: BindingType::Buffer {
                         ty: BufferBindingType::Uniform,
                         has_dynamic_offset: true,
-                        min_binding_size: BufferSize::new(ViewUniform::std140_size_static() as u64),
+                        min_binding_size: Some(ViewUniform::min_size()),
                     },
                     count: None,
                 },
@@ -291,7 +288,7 @@ impl FromWorld for MeshPipeline {
                     ty: BindingType::Buffer {
                         ty: BufferBindingType::Uniform,
                         has_dynamic_offset: true,
-                        min_binding_size: BufferSize::new(GpuLights::std140_size_static() as u64),
+                        min_binding_size: Some(GpuLights::min_size()),
                     },
                     count: None,
                 },
@@ -344,10 +341,9 @@ impl FromWorld for MeshPipeline {
                     ty: BindingType::Buffer {
                         ty: clustered_forward_buffer_binding_type,
                         has_dynamic_offset: false,
-                        // NOTE (when no storage buffers): Static size for uniform buffers.
-                        // GpuPointLight has a padded size of 64 bytes, so 16384 / 64 = 256
-                        // point lights max
-                        min_binding_size: cluster_min_binding_size,
+                        min_binding_size: Some(GpuPointLights::min_size(
+                            clustered_forward_buffer_binding_type,
+                        )),
                     },
                     count: None,
                 },
@@ -358,9 +354,11 @@ impl FromWorld for MeshPipeline {
                     ty: BindingType::Buffer {
                         ty: clustered_forward_buffer_binding_type,
                         has_dynamic_offset: false,
-                        // NOTE (when no storage buffers): With 256 point lights max, indices
-                        // need 8 bits so use u8
-                        min_binding_size: cluster_min_binding_size,
+                        min_binding_size: Some(
+                            ViewClusterBindings::min_size_cluster_light_index_lists(
+                                clustered_forward_buffer_binding_type,
+                            ),
+                        ),
                     },
                     count: None,
                 },
@@ -371,12 +369,11 @@ impl FromWorld for MeshPipeline {
                     ty: BindingType::Buffer {
                         ty: clustered_forward_buffer_binding_type,
                         has_dynamic_offset: false,
-                        // NOTE (when no storage buffers): The offset needs to address 16384
-                        // indices, which needs 14 bits. The count can be at most all 256 lights
-                        // so 8 bits.
-                        // NOTE: Pack the offset into the upper 19 bits and the count into the
-                        // lower 13 bits.
-                        min_binding_size: cluster_min_binding_size,
+                        min_binding_size: Some(
+                            ViewClusterBindings::min_size_cluster_offsets_and_counts(
+                                clustered_forward_buffer_binding_type,
+                            ),
+                        ),
                     },
                     count: None,
                 },
@@ -390,7 +387,7 @@ impl FromWorld for MeshPipeline {
             ty: BindingType::Buffer {
                 ty: BufferBindingType::Uniform,
                 has_dynamic_offset: true,
-                min_binding_size: BufferSize::new(MeshUniform::std140_size_static() as u64),
+                min_binding_size: Some(MeshUniform::min_size()),
             },
             count: None,
         };
@@ -688,7 +685,7 @@ pub fn queue_mesh_bind_group(
             skinned: None,
         };
 
-        if let Some(skinned_joints_buffer) = skinned_mesh_uniform.buffer.uniform_buffer() {
+        if let Some(skinned_joints_buffer) = skinned_mesh_uniform.buffer.buffer() {
             mesh_bind_group.skinned = Some(render_device.create_bind_group(&BindGroupDescriptor {
                 entries: &[
                     BindGroupEntry {
@@ -712,9 +709,15 @@ pub fn queue_mesh_bind_group(
     }
 }
 
+// NOTE: This is using BufferVec because it is using a trick to allow a fixed-size array
+// in a uniform buffer to be used like a variable-sized array by only writing the valid data
+// into the buffer, knowing the number of valid items starting from the dynamic offset, and
+// ignoring the rest, whether they're valid for other dynamic offsets or not. This trick may
+// be supported later in encase, and then we should make use of it.
+
 #[derive(Default)]
 pub struct SkinnedMeshUniform {
-    pub buffer: UniformVec<Mat4>,
+    pub buffer: BufferVec<Mat4>,
 }
 
 pub fn prepare_skinned_meshes(

crates/bevy_sprite/src/mesh2d/mesh.rs
@@ -11,7 +11,7 @@ use bevy_render::{
     render_asset::RenderAssets,
     render_component::{ComponentUniforms, DynamicUniformIndex, UniformComponentPlugin},
     render_phase::{EntityRenderCommand, RenderCommandResult, TrackedRenderPass},
-    render_resource::{std140::AsStd140, *},
+    render_resource::*,
     renderer::{RenderDevice, RenderQueue},
     texture::{BevyDefault, GpuImage, Image, TextureFormatPixelInfo},
     view::{ComputedVisibility, ExtractedView, ViewUniform, ViewUniformOffset, ViewUniforms},
@@ -71,7 +71,7 @@ impl Plugin for Mesh2dRenderPlugin {
     }
 }
 
-#[derive(Component, AsStd140, Clone)]
+#[derive(Component, ShaderType, Clone)]
 pub struct Mesh2dUniform {
     pub transform: Mat4,
     pub inverse_transpose_model: Mat4,
@@ -134,7 +134,7 @@ impl FromWorld for Mesh2dPipeline {
                     ty: BindingType::Buffer {
                         ty: BufferBindingType::Uniform,
                         has_dynamic_offset: true,
-                        min_binding_size: BufferSize::new(ViewUniform::std140_size_static() as u64),
+                        min_binding_size: Some(ViewUniform::min_size()),
                     },
                     count: None,
                 },
@@ -149,7 +149,7 @@ impl FromWorld for Mesh2dPipeline {
                 ty: BindingType::Buffer {
                     ty: BufferBindingType::Uniform,
                     has_dynamic_offset: true,
-                    min_binding_size: BufferSize::new(Mesh2dUniform::std140_size_static() as u64),
+                    min_binding_size: Some(Mesh2dUniform::min_size()),
                 },
                 count: None,
             }],

crates/bevy_crevice/Cargo.toml
@@ -1,36 +1,0 @@
-[package]
-name = "bevy_crevice"
-description = "Create GLSL-compatible versions of structs with explicitly-initialized padding (Bevy version)"
-version = "0.8.0-dev"
-edition = "2021"
-authors = ["Lucien Greathouse <me@lpghatguy.com>"]
-documentation = "https://docs.rs/crevice"
-homepage = "https://github.com/LPGhatguy/crevice"
-repository = "https://github.com/bevyengine/bevy"
-readme = "README.md"
-keywords = ["glsl", "std140", "std430"]
-license = "MIT OR Apache-2.0"
-# resolver = "2"
-
-# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
-
-[features]
-default = ["std"]
-std = []
-
-# [workspace]
-# members = ["crevice-derive", "crevice-tests"]
-# default-members = ["crevice-derive", "crevice-tests"]
-
-[dependencies]
-bevy-crevice-derive = { version = "0.8.0-dev", path = "bevy-crevice-derive" }
-
-bytemuck = "1.4.1"
-mint = "0.5.8"
-
-cgmath = { version = "0.18.0", optional = true }
-glam = { version = "0.20.0", features = ["mint"], optional = true }
-nalgebra = { version = "0.30.0", features = ["mint"], optional = true }
-
-[dev-dependencies]
-insta = "0.16.1"

crates/bevy_crevice/LICENSE-APACHE
@@ -1,201 +1,0 @@
-i                                 Apache License
-                           Version 2.0, January 2004
-                        http://www.apache.org/licenses/
-
-   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
-
-   1. Definitions.
-
-      "License" shall mean the terms and conditions for use, reproduction,
-      and distribution as defined by Sections 1 through 9 of this document.
-
-      "Licensor" shall mean the copyright owner or entity authorized by
-      the copyright owner that is granting the License.
-
-      "Legal Entity" shall mean the union of the acting entity and all
-      other entities that control, are controlled by, or are under common
-      control with that entity. For the purposes of this definition,
-      "control" means (i) the power, direct or indirect, to cause the
-      direction or management of such entity, whether by contract or
-      otherwise, or (ii) ownership of fifty percent (50%) or more of the
-      outstanding shares, or (iii) beneficial ownership of such entity.
-
-      "You" (or "Your") shall mean an individual or Legal Entity
-      exercising permissions granted by this License.
-
-      "Source" form shall mean the preferred form for making modifications,
-      including but not limited to software source code, documentation
-      source, and configuration files.
-
-      "Object" form shall mean any form resulting from mechanical
-      transformation or translation of a Source form, including but
-      not limited to compiled object code, generated documentation,
-      and conversions to other media types.
-
-      "Work" shall mean the work of authorship, whether in Source or
-      Object form, made available under the License, as indicated by a
-      copyright notice that is included in or attached to the work
-      (an example is provided in the Appendix below).
-
-      "Derivative Works" shall mean any work, whether in Source or Object
-      form, that is based on (or derived from) the Work and for which the
-      editorial revisions, annotations, elaborations, or other modifications
-      represent, as a whole, an original work of authorship. For the purposes
-      of this License, Derivative Works shall not include works that remain
-      separable from, or merely link (or bind by name) to the interfaces of,
-      the Work and Derivative Works thereof.
-
-      "Contribution" shall mean any work of authorship, including
-      the original version of the Work and any modifications or additions
-      to that Work or Derivative Works thereof, that is intentionally
-      submitted to Licensor for inclusion in the Work by the copyright owner
-      or by an individual or Legal Entity authorized to submit on behalf of
-      the copyright owner. For the purposes of this definition, "submitted"
-      means any form of electronic, verbal, or written communication sent
-      to the Licensor or its representatives, including but not limited to
-      communication on electronic mailing lists, source code control systems,
-      and issue tracking systems that are managed by, or on behalf of, the
-      Licensor for the purpose of discussing and improving the Work, but
-      excluding communication that is conspicuously marked or otherwise
-      designated in writing by the copyright owner as "Not a Contribution."
-
-      "Contributor" shall mean Licensor and any individual or Legal Entity
-      on behalf of whom a Contribution has been received by Licensor and
-      subsequently incorporated within the Work.
-
-   2. Grant of Copyright License. Subject to the terms and conditions of
-      this License, each Contributor hereby grants to You a perpetual,
-      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
-      copyright license to reproduce, prepare Derivative Works of,
-      publicly display, publicly perform, sublicense, and distribute the
-      Work and such Derivative Works in Source or Object form.
-
-   3. Grant of Patent License. Subject to the terms and conditions of
-      this License, each Contributor hereby grants to You a perpetual,
-      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
-      (except as stated in this section) patent license to make, have made,
-      use, offer to sell, sell, import, and otherwise transfer the Work,
-      where such license applies only to those patent claims licensable
-      by such Contributor that are necessarily infringed by their
-      Contribution(s) alone or by combination of their Contribution(s)
-      with the Work to which such Contribution(s) was submitted. If You
-      institute patent litigation against any entity (including a
-      cross-claim or counterclaim in a lawsuit) alleging that the Work
-      or a Contribution incorporated within the Work constitutes direct
-      or contributory patent infringement, then any patent licenses
-      granted to You under this License for that Work shall terminate
-      as of the date such litigation is filed.
-
-   4. Redistribution. You may reproduce and distribute copies of the
-      Work or Derivative Works thereof in any medium, with or without
-      modifications, and in Source or Object form, provided that You
-      meet the following conditions:
-
-      (a) You must give any other recipients of the Work or
-          Derivative Works a copy of this License; and
-
-      (b) You must cause any modified files to carry prominent notices
-          stating that You changed the files; and
-
-      (c) You must retain, in the Source form of any Derivative Works
-          that You distribute, all copyright, patent, trademark, and
-          attribution notices from the Source form of the Work,
-          excluding those notices that do not pertain to any part of
-          the Derivative Works; and
-
-      (d) If the Work includes a "NOTICE" text file as part of its
-          distribution, then any Derivative Works that You distribute must
-          include a readable copy of the attribution notices contained
-          within such NOTICE file, excluding those notices that do not
-          pertain to any part of the Derivative Works, in at least one
-          of the following places: within a NOTICE text file distributed
-          as part of the Derivative Works; within the Source form or
-          documentation, if provided along with the Derivative Works; or,
-          within a display generated by the Derivative Works, if and
-          wherever such third-party notices normally appear. The contents
-          of the NOTICE file are for informational purposes only and
-          do not modify the License. You may add Your own attribution
-          notices within Derivative Works that You distribute, alongside
-          or as an addendum to the NOTICE text from the Work, provided
-          that such additional attribution notices cannot be construed
-          as modifying the License.
-
-      You may add Your own copyright statement to Your modifications and
-      may provide additional or different license terms and conditions
-      for use, reproduction, or distribution of Your modifications, or
-      for any such Derivative Works as a whole, provided Your use,
-      reproduction, and distribution of the Work otherwise complies with
-      the conditions stated in this License.
-
-   5. Submission of Contributions. Unless You explicitly state otherwise,
-      any Contribution intentionally submitted for inclusion in the Work
-      by You to the Licensor shall be under the terms and conditions of
-      this License, without any additional terms or conditions.
-      Notwithstanding the above, nothing herein shall supersede or modify
-      the terms of any separate license agreement you may have executed
-      with Licensor regarding such Contributions.
-
-   6. Trademarks. This License does not grant permission to use the trade
-      names, trademarks, service marks, or product names of the Licensor,
-      except as required for reasonable and customary use in describing the
-      origin of the Work and reproducing the content of the NOTICE file.
-
-   7. Disclaimer of Warranty. Unless required by applicable law or
-      agreed to in writing, Licensor provides the Work (and each
-      Contributor provides its Contributions) on an "AS IS" BASIS,
-      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-      implied, including, without limitation, any warranties or conditions
-      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
-      PARTICULAR PURPOSE. You are solely responsible for determining the
-      appropriateness of using or redistributing the Work and assume any
-      risks associated with Your exercise of permissions under this License.
-
-   8. Limitation of Liability. In no event and under no legal theory,
-      whether in tort (including negligence), contract, or otherwise,
-      unless required by applicable law (such as deliberate and grossly
-      negligent acts) or agreed to in writing, shall any Contributor be
-      liable to You for damages, including any direct, indirect, special,
-      incidental, or consequential damages of any character arising as a
-      result of this License or out of the use or inability to use the
-      Work (including but not limited to damages for loss of goodwill,
-      work stoppage, computer failure or malfunction, or any and all
-      other commercial damages or losses), even if such Contributor
-      has been advised of the possibility of such damages.
-
-   9. Accepting Warranty or Additional Liability. While redistributing
-      the Work or Derivative Works thereof, You may choose to offer,
-      and charge a fee for, acceptance of support, warranty, indemnity,
-      or other liability obligations and/or rights consistent with this
-      License. However, in accepting such obligations, You may act only
-      on Your own behalf and on Your sole responsibility, not on behalf
-      of any other Contributor, and only if You agree to indemnify,
-      defend, and hold each Contributor harmless for any liability
-      incurred by, or claims asserted against, such Contributor by reason
-      of your accepting any such warranty or additional liability.
-
-   END OF TERMS AND CONDITIONS
-
-   APPENDIX: How to apply the Apache License to your work.
-
-      To apply the Apache License to your work, attach the following
-      boilerplate notice, with the fields enclosed by brackets "{}"
-      replaced with your own identifying information. (Don't include
-      the brackets!)  The text should be enclosed in the appropriate
-      comment syntax for the file format. We also recommend that a
-      file or class name and description of purpose be included on the
-      same "printed page" as the copyright notice for easier
-      identification within third-party archives.
-
-   Copyright {yyyy} {name of copyright owner}
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.

crates/bevy_crevice/LICENSE-MIT
@@ -1,19 +1,0 @@
-Copyright (c) 2020 Lucien Greathouse
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.

crates/bevy_crevice/README.tpl
@@ -1,25 +1,0 @@
-# Crevice
-
-{{readme}}
-
-[std140::AsStd140]: https://docs.rs/crevice/latest/crevice/std140/trait.AsStd140.html
-[std140::AsStd140::as_std140]: https://docs.rs/crevice/latest/crevice/std140/trait.AsStd140.html#method.as_std140
-[std140::Std140::as_bytes]: https://docs.rs/crevice/latest/crevice/std140/trait.Std140.html#method.as_bytes
-[std140::Writer]: https://docs.rs/crevice/latest/crevice/std140/struct.Writer.html
-
-[`std::io::Write`]: https://doc.rust-lang.org/stable/std/io/trait.Write.html
-
-[`bytemuck::Pod`]: https://docs.rs/bytemuck/latest/bytemuck/trait.Pod.html
-[`bytemuck::Zeroable`]: https://docs.rs/bytemuck/latest/bytemuck/trait.Zeroable.html
-
-## License
-
-Licensed under either of
-
- * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
- * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
-
-at your option.
-
-### Contribution
-Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.

crates/bevy_crevice/bevy-crevice-derive/Cargo.toml
@@ -1,27 +1,0 @@
-[package]
-name = "bevy-crevice-derive"
-description = "Derive crate for the 'crevice' crate (Bevy version)"
-version = "0.8.0-dev"
-edition = "2018"
-authors = ["Lucien Greathouse <me@lpghatguy.com>"]
-documentation = "https://docs.rs/crevice-derive"
-homepage = "https://github.com/LPGhatguy/crevice"
-repository = "https://github.com/bevyengine/bevy"
-license = "MIT OR Apache-2.0"
-
-[features]
-default = []
-
-# Enable methods that let you introspect into the generated structs.
-debug-methods = []
-
-[lib]
-proc-macro = true
-
-# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
-
-[dependencies]
-syn = "1.0.40"
-quote = "1.0.7"
-proc-macro2 = "1.0.21"
-bevy_macro_utils = { path = "../../bevy_macro_utils", version = "0.8.0-dev" }

crates/bevy_crevice/bevy-crevice-derive/src/glsl.rs
@@ -1,48 +1,0 @@
-use bevy_macro_utils::get_named_struct_fields;
-use proc_macro2::{Literal, TokenStream};
-use quote::quote;
-use syn::{parse_quote, DeriveInput, Path};
-
-pub fn emit(input: DeriveInput) -> TokenStream {
-    let bevy_crevice_path = crate::bevy_crevice_path();
-
-    let fields = match get_named_struct_fields(&input.data) {
-        Ok(fields) => fields,
-        Err(e) => return e.into_compile_error(),
-    };
-
-    let base_trait_path: Path = parse_quote!(#bevy_crevice_path::glsl::Glsl);
-    let struct_trait_path: Path = parse_quote!(#bevy_crevice_path::glsl::GlslStruct);
-
-    let name = input.ident;
-    let name_str = Literal::string(&name.to_string());
-
-    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-
-    let glsl_fields = fields.named.iter().map(|field| {
-        let field_ty = &field.ty;
-        let field_name_str = Literal::string(&field.ident.as_ref().unwrap().to_string());
-        let field_as = quote! {<#field_ty as #bevy_crevice_path::glsl::GlslArray>};
-
-        quote! {
-            s.push_str("\t");
-            s.push_str(#field_as::NAME);
-            s.push_str(" ");
-            s.push_str(#field_name_str);
-            <#field_as::ArraySize as #bevy_crevice_path::glsl::DimensionList>::push_to_string(s);
-            s.push_str(";\n");
-        }
-    });
-
-    quote! {
-        unsafe impl #impl_generics #base_trait_path for #name #ty_generics #where_clause {
-            const NAME: &'static str = #name_str;
-        }
-
-        unsafe impl #impl_generics #struct_trait_path for #name #ty_generics #where_clause {
-            fn enumerate_fields(s: &mut String) {
-                #( #glsl_fields )*
-            }
-        }
-    }
-}

commit 2ee7f62bcec050419faf6788f90c8455e3b90af9

Use uniform buffer usage for `SkinnedMeshUniform` instead of all usages (#4816)
# Objective

fixes #4811 (caused by #4339 [[exact change](https://github.com/bevyengine/bevy/pull/4339/files#diff-4bf3ed03d4129aad9f5678ba19f9b14ee8e3e61d6f6365e82197b01c74468b10R712-R721)] - where the buffer type has been changed from `UniformVec` to `BufferVec`)

## Solution

Use uniform buffer usage for `SkinnedMeshUniform` instead of all usages due to the `Default` derive.

crates/bevy_pbr/src/render/mesh.rs
@@ -715,9 +715,16 @@ pub fn queue_mesh_bind_group(
 // ignoring the rest, whether they're valid for other dynamic offsets or not. This trick may
 // be supported later in encase, and then we should make use of it.
 
-#[derive(Default)]
 pub struct SkinnedMeshUniform {
     pub buffer: BufferVec<Mat4>,
+}
+
+impl Default for SkinnedMeshUniform {
+    fn default() -> Self {
+        Self {
+            buffer: BufferVec::new(BufferUsages::UNIFORM),
+        }
+    }
 }
 
 pub fn prepare_skinned_meshes(

crates/bevy_render/src/render_resource/buffer_vec.rs
@@ -14,23 +14,14 @@ pub struct BufferVec<T: Pod> {
     buffer_usage: BufferUsages,
 }
 
-impl<T: Pod> Default for BufferVec<T> {
-    fn default() -> Self {
+impl<T: Pod> BufferVec<T> {
+    pub const fn new(buffer_usage: BufferUsages) -> Self {
         Self {
             values: Vec::new(),
             buffer: None,
             capacity: 0,
-            buffer_usage: BufferUsages::all(),
             item_size: std::mem::size_of::<T>(),
-        }
-    }
-}
-
-impl<T: Pod> BufferVec<T> {
-    pub fn new(buffer_usage: BufferUsages) -> Self {
-        Self {
             buffer_usage,
-            ..Default::default()
         }
     }
 